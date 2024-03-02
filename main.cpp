ace9fae09e001c0e050a08f21d81d3cfd6925b57
Sydney Marshall <tmq265@ut.utm.edu>
2024-03-01 20:46:39 -0600

    documentation edit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file main.cpp
+ * @author Kimble Culley and Sydney Marshall
+ * @date 2024-03-01
+ * @brief Performs Image Stacking 
+ * 
+ * Initializes a stacker onject and performs the image stacking using the stacker class. 
+ The tests include capturing user input for the image name and the number of images to stack.
+ * The main function interacts with the Stacker class, calling methods to read individual PPM 
+files,stack multiple images, and write the resulting stacked image to a new PPM file.
+ */
+
 
 #include <iostream>
 #include <fstream>
@@ -9,33 +21,41 @@
 using namespace std;
 
 int main() {
-
+  // stacker class
   Stacker photo;
 
+  //declares variables for user input
   string inFileName;
   stringstream temp;
   string temp2;
   int numImages;
 
-  
+  //prompts user to enter the image to stack
   cout << "Please enter the image you wish to stack: ";
 
+  // gets user input for the name of the image
   cin >> inFileName;
 
+  // file path for first image 
   temp << "ppms/ppms/" << inFileName << "/" << inFileName << "_" << "001" << ".ppm";
   temp2 = temp.str();
-  cout << "Please enter the number of images: ";
 
+  cout << "Please enter the number of images: ";
+  // gets user input for number of images
   cin >> numImages;
 
   cout << "Stacking images: " << endl;
 
-
+  // reads first image to make the stacker object 
   photo.readFile(temp2);
 
+  // stacks num of images using stacker class
   photo.stackImage(inFileName, numImages);
 
+  // makes output file name for final image 
   string outFileName = inFileName + ".ppm";
+
+  //writes final image to a new file 
   photo.outFile(outFileName);
 
   
